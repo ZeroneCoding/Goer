8af41ada29af4c2e28cb76dea60f6ebcee539c6d
ZeroneCoding <1043481495@qq.com>
2016-05-24 19:21:21 +0800

    init the project

---
+++ Goer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Goer
+//
+//  Created by 李银 on 16/5/24.
+//  Copyright © 2016年 liyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Goer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Goer
+//
+//  Created by 李银 on 16/5/24.
+//  Copyright © 2016年 liyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
